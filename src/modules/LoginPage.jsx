ca1a9aadef6edeca0400a9439ce694c47b1c6053
flexike <flexike@ukr.net>
2023-02-19 18:14:37 +0200

    +register page, +few icons, +few inputs, +country_flags = idk how it works

--- src/modules/LoginPage.jsx
+++ src/modules/LoginPage.jsx
@@ -27,126 +27,194 @@ const LoginPage = () => {
     };
 
     return (
-        <Container
-            maxWidth="md"
-            style={{
-                border: '1px solid black',
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
             }}
         >
-            <div
+            <Container
+                maxWidth="md"
                 style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
+                    border: '1px solid black',
+                    borderRadius: '10px',
+                    height: '700px',
                 }}
             >
-                <img src={logo} alt="1" width={300} height={300} />
-                {/*login*/}
                 <div
                     style={{
                         display: 'flex',
-                        flexDirection: 'column',
-                        margin: '0px 50px',
+                        justifyContent: 'center',
+                        alignItems: 'center',
                     }}
                 >
-                    <h2>Login</h2>
-                    <TextField
-                        margin="normal"
-                        id="login"
-                        label="Login"
-                        variant="outlined"
-                        InputProps={{
-                            endAdornment: (
-                                <InputAdornment position="end">
-                                    <AccountCircle />
-                                </InputAdornment>
-                            ),
+                    <img
+                        src={logo}
+                        alt="1"
+                        style={{
+                            width: '60%',
+                            objectFit: 'contain',
                         }}
                     />
-                    {/*password*/}
-                    <TextField
-                        margin="normal"
-                        id="password"
-                        label="Password"
-                        variant="outlined"
-                        type={showPassword ? 'text' : 'password'}
-                        InputProps={{
-                            endAdornment: (
-                                <InputAdornment position="end">
-                                    <IconButton
-                                        aria-label="toggle password visibility"
-                                        onClick={handleClickShowPassword}
-                                        onMouseDown={handleMouseDownPassword}
-                                    >
-                                        {showPassword ? (
-                                            <VisibilityOff />
-                                        ) : (
-                                            <Visibility />
-                                        )}
-                                    </IconButton>
-                                </InputAdornment>
-                            ),
-                        }}
-                    />
+                    {/*login*/}
                     <div
                         style={{
                             display: 'flex',
-                            flexDirection: 'row',
-                            margin: '10px 0px',
+                            width: '40%',
+                            flexDirection: 'column',
+                            margin: '70px 0px 0px 50px',
                         }}
                     >
-                        <Button
-                            variant="contained"
-                            style={{ marginRight: '25px' }}
+                        <h2
+                            style={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                marginBottom: '80px',
+                            }}
                         >
                             Login
-                        </Button>
-                        <Button variant="text" size="small">
+                        </h2>
+                        <TextField
+                            margin="normal"
+                            id="login"
+                            label="Login"
+                            variant="outlined"
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment
+                                        position="end"
+                                        style={{
+                                            marginRight: '7.5px',
+                                        }}
+                                    >
+                                        <AccountCircle />
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
+                        {/*password*/}
+                        <TextField
+                            margin="normal"
+                            id="password"
+                            label="Password"
+                            variant="outlined"
+                            type={showPassword ? 'text' : 'password'}
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={handleClickShowPassword}
+                                            onMouseDown={
+                                                handleMouseDownPassword
+                                            }
+                                        >
+                                            {showPassword ? (
+                                                <VisibilityOff />
+                                            ) : (
+                                                <Visibility />
+                                            )}
+                                        </IconButton>
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
+                        <a
+                            href="/recovery"
+                            style={{
+                                textAlign: 'right',
+                                fontSize: '0.7rem',
+                                color: 'grey',
+                                textDecoration: 'none',
+                            }}
+                        >
                             Password Recovery
-                        </Button>
+                        </a>
+                        <div
+                            style={{
+                                display: 'flex',
+                                marginTop: '20px',
+                                width: '100%',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Button
+                                variant="contained"
+                                style={{
+                                    width: '60%',
+                                    fontFamily: 'Poppins',
+                                    letterSpacing: '1.2px',
+                                }}
+                            >
+                                Login
+                            </Button>
+                        </div>
+
+                        <div
+                            style={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                flexDirection: 'column',
+                                margin: '50px 0px 0px 0px',
+                                position: 'relative',
+                                bottom: 0,
+                            }}
+                        >
+                            <p
+                                style={{
+                                    color: 'grey',
+                                    textAlign: 'center',
+                                }}
+                            >
+                                If you don't have an account{' '}
+                                <a
+                                    href="/register"
+                                    style={{
+                                        textDecoration: 'none',
+                                        color: 'blue',
+                                    }}
+                                >
+                                    Register
+                                </a>
+                            </p>
+                            <p
+                                style={{
+                                    marginTop: '25px',
+                                    marginBottom: '0px',
+                                }}
+                            >
+                                <a
+                                    href="/contacts"
+                                    style={{
+                                        textDecoration: 'none',
+                                        color: 'grey',
+                                    }}
+                                >
+                                    Contact Us
+                                </a>
+                            </p>
+                            <p>
+                                <a
+                                    href="/contacts"
+                                    style={{
+                                        textDecoration: 'none',
+                                        color: 'grey',
+                                        fontSize: '0.8rem',
+                                        margin: '0',
+                                    }}
+                                >
+                                    © Atlantis 2022-2023
+                                </a>
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    flexDirection: 'column',
-                    marginTop: '50px',
-                }}
-            >
-                <p style={{ color: 'grey' }}>
-                    If you don't have an account{' '}
-                    <a
-                        href="1"
-                        style={{ textDecoration: 'none', color: 'blue' }}
-                    >
-                        Register
-                    </a>
-                </p>
-                <p style={{ marginTop: '25px', marginBottom: '0px' }}>
-                    <a
-                        href="2"
-                        style={{ textDecoration: 'none', color: 'grey' }}
-                    >
-                        Contact Us
-                    </a>
-                </p>
-                <p>
-                    <a
-                        href="3"
-                        style={{
-                            textDecoration: 'none',
-                            color: 'grey',
-                            fontSize: '0.8rem',
-                        }}
-                    >
-                        © Atlantis 2022-2023
-                    </a>
-                </p>
-            </div>
-        </Container>
+            </Container>
+        </Box>
     );
 };
 
